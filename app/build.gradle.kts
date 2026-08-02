49a0b0dbab727dd0afe7a06b49aadcba083f3ac9
Luis Ángel Landa Cuevas <wiwyzho@192.168.1.26>
2026-08-02 14:27:48 -0600

    build: bump version to 1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,8 +84,8 @@ android {
         applicationId = "com.wiwymusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 51
-        versionName = "1.1.4"
+        versionCode = 52
+        versionName = "1.1.5"
 //        versionName = "3.0.2-$gitCommit"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
