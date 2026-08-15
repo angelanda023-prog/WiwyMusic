ae7e3b83079a23f56a094d4cd28f2ab79186b15a
Luis Ángel Landa Cuevas <wiwyzho@192.168.1.28>
2026-08-15 16:31:15 -0600

    build: prepare Android v1.1.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,8 +84,8 @@ android {
         applicationId = "com.wiwymusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 63
-        versionName = "1.1.16"
+        versionCode = 64
+        versionName = "1.1.17"
 //        versionName = "3.0.2-$gitCommit"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
